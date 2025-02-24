41335761caf21ccc6227e1529abbe4b60eb130e3
Edder19 <eaderandres021@gmail.com>
2025-02-24 18:36:41 -0500

    class Date

---
+++ POOconcepts/POOconcepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using POOconcepts;
+try
+{
+
+    var date1 = new Date();
+    var date2 = new Date(1947, 10, 23);
+    var date3 = new Date(2025, 2, 28);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
